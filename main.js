0d6827beaf1d42ee2390614aaa141b43b68ef959
sueyesi <publisher724@gmail.com>
2024-07-11 19:09:45 +0900

    second commit

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 
 const API_KEY = `6aba51ea81af491aa6793d096958527f`;
 let newsList = [];
-let category ='';
 //뉴스를 가져오는 함수 설정
 const getLatestNews = async() =>{ 
     //const url = new URL( `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`); 
-    const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?category=${category}`); 
+    const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines`); 
     const response = await fetch(url); // await  fetch함수가 완료될 때까지 기다려준다
     const data = await response.json();// json파일형식
     newsList = data.articles;
